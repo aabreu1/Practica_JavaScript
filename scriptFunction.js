f94a52302b408c978b784a4eb405845d7f0e3154
Andres Abreu <aabreu@ingesoft.local>
2016-04-29 13:54:22 -0430

    añadido archivo .js

---
+++ scriptFunction.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<!--<script src="carpeta/archivo.js"></script>-->
+<head>
+<title>Demostracion Bucle</title><!-- CABEZERA-->
+	
+</head>
+
+<body>
+	<script language="JavaScript">
+	
+	function confirmar()
+	{
+		confirm ("Confirmar por el usuario");
+	}
+
+
+	</script>
+</body>
+</html>
+
